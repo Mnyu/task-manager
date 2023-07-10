45e3ea58c0e9c1466305e44a7d3e7fd6c7c7fa9d
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-10 16:26:56 +0530

    response for non existing routes - 404

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tasksRouter = require('./routes/tasks');
 const connectDB = require('./database/connect');
+const notFound = require('./middleware/not-found');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ const port = 3000;
 app.use(express.static('./public')); // for static assets from public folder
 app.use(express.json()); // for request body json
 app.use('/api/v1/tasks', tasksRouter); // for routes
+app.use(notFound); // for non existing routes - 404
 
 const start = async () => {
   try {
