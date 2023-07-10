58c11d58db46d922eedf34bca14e765e1f508aef
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-10 19:01:21 +0530

    error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const tasksRouter = require('./routes/tasks');
 const connectDB = require('./database/connect');
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.static('./public')); // for static assets from public folder
 app.use(express.json()); // for request body json
 app.use('/api/v1/tasks', tasksRouter); // for routes
 app.use(notFound); // for non existing routes - 404
+app.use(errorHandlerMiddleware); // to add custom error handling mechanism
 
 const start = async () => {
   try {
